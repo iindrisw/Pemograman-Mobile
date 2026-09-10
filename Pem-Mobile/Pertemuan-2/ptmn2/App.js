42cdf934676311ce3d60e2e6c30997762b7a29e9
iindrisw <indrisetiawati0830@gmail.com>
2026-09-10 12:05:41 +0700

    tugas indri Pemograman Mobile

---
+++ Pem-Mobile/Pertemuan-2/ptmn2/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app halo indri!</Text>
+      <Text>Nama lengkap : Indri Setiawati</Text>
+      <Text>Tempat, Tanggal lahir: Cirebon, 30 Agustus 2006</Text>
+      <Text>Cita-Cita : menjadi org kaya raya </Text>
+      <Text>Rencana Hidup : adadeh </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
